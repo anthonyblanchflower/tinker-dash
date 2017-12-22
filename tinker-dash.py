999a2e3474bb32412194d02704754745b9941c06
anthony blanchflower <anthony.blanchflower@comparethemarket.com>
2017-12-22 11:31:39 +0000

    Remove TimeoutException and add driver.refresh() to ensure tabs are regularly updated

--- tinker-dash.py
+++ tinker-dash.py
@@ -1,10 +1,9 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import TimeoutException
 
 DASHBOARD_LIST = 'the-dashboard-list.txt'
-TAB_ROTATION_PERIOD = 5  # 5 second tab rotation
+TAB_ROTATION_PERIOD = 15  # 15 second tab rotation
 
 
 def pull_list(list_file):
@@ -21,18 +20,13 @@ def open_dashboard(url_list, driver):
     tab_ordinal = 1
 
     for dashboard_url in url_list:
-        # 10 second limit on loading URLs
-        try:
-            driver.set_page_load_timeout(10)
-            driver.get(dashboard_url)
-            if tab_ordinal <= max_tabs:
-                browser_tab = 'tab' + str(tab_ordinal + 1)
-                tab_open_command = "window.open('about:blank', '" + browser_tab + "');"
-                driver.execute_script(tab_open_command)
-                driver.switch_to.window(browser_tab)
-                tab_ordinal += 1
-        except TimeoutException:
-            pass
+        driver.get(dashboard_url)
+        if tab_ordinal <= max_tabs:
+            browser_tab = 'tab' + str(tab_ordinal + 1)
+            tab_open_command = "window.open('about:blank', '" + browser_tab + "');"
+            driver.execute_script(tab_open_command)
+            driver.switch_to.window(browser_tab)
+            tab_ordinal += 1
 
 
 def rotate_dashboard(url_list, driver):
@@ -42,6 +36,7 @@ def rotate_dashboard(url_list, driver):
     while tab_ordinal <= max_tabs:
         browser_tab = 'tab' + str(tab_ordinal + 1)
         driver.switch_to.window(browser_tab)
+        driver.refresh()
         time.sleep(TAB_ROTATION_PERIOD)
         tab_ordinal += 1
 
